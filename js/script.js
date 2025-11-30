ad2d198855f5bf91f060de25aba801e1e0ab8e5b
Jesus Borja <jesus.borja.hern@gmail.com>
2025-11-30 03:21:01 -0600

    Introduce time to demo steps

--- js/script.js
+++ js/script.js
@@ -334,6 +334,7 @@ async function addStepToDemo() {
     const container = document.getElementById("steps-container");
     const id = container.childElementCount + 1;
     const div = document.createElement("div");
+
     div.classList.add(
         "steps-wrapper",
         "d-flex",
@@ -342,23 +343,32 @@ async function addStepToDemo() {
         "w-100"
     );
     div.id = `steps-wrapper-${id}`;
+
     const select = document.createElement("select");
     select.classList.add("steps-select", "devices-select");
     select.name = "steps-select";
     select.id = `steps-select-${id}`;
 
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = "1s";
+    input.classList.add("form-control");
+
     const btn = document.createElement("button");
     btn.type = "button";
-    btn.innerText = "Borrar";
+    btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+</svg>`;
     btn.id = `step-remove-${id}`;
-    btn.setAttribute("target-id", div.id);
+    btn.classList.add("btn", "btn-secondary");
+
     btn.addEventListener("click", () => {
         const parent = document.getElementById(div.id);
         parent.remove();
     });
-    btn.classList.add("btn", "btn-secondary");
 
     div.appendChild(select);
+    div.appendChild(input);
     div.appendChild(btn);
     container.appendChild(div);
     await loadSteps(select.id);
